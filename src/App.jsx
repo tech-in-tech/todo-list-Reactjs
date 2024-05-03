705c6cba3b5c237abb20a420f0bb7f2e169d124c
tech-in-tech <anubhavsharma06dec@gmail.com>
2024-05-03 14:50:35 +0530

    add chenkbox functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,69 @@
+import { useState } from "react"
 import Navbar from "./Components/Navbar"
+import { v4 as uuidv4 } from "uuid"
+
 
 function App() {
+  const [todo, setTodo] = useState("")
+  const [todos, setTodos] = useState([])
+
+
+  const handleEdit = () => {
+
+  }
+  const handleDelete = () => {
+
+  }
+  const handleAdd = () => {
+    setTodos([...todos, {id: uuidv4(), todo, isCompleted: false }])
+    setTodo("")
+    console.log(todos)
+  }
+
+
+  const handleChange = (e) => {
+    setTodo(e.target.value)
+  }
+
+  const handleCheckbox  = (e) => {
+    let id = e.target.name
+    let index = todos.findIndex(item=>{
+      return item.id === id;
+    })
+    let newTodos = [...todos];
+    newTodos[index].isCompleted = !newTodos[index].isCompleted;
+    setTodos(newTodos)
+  }
+  
+
 
   return (
     <>
-      <Navbar/>
+      <Navbar />
       <div className="container mx-auto my-5 bg-violet-100 rounded-xl min-h-[80vh] p-5">
-      <div className="addTodo">
-        <h2 className="text-lg font-bold">Add a Todo</h2>
-        <input type="text" name="" id="" />
-        <button className="bg-violet-800 hover:bg-violet-950 text-white font-bold py-2 px-4  border rounded-xl mx-3 cursor-pointer transition-all">Save</button>
-      </div>
-      <h2 className="text-lg font-bold">Your Todos</h2>
-      <div className="todos">
-        <div className="todo flex">
-          <div className="text">Lorem ipsum dolor sit amet consectetur adipisicing.</div>
-          <div className="buttons">
-            <button className="bg-violet-800 hover:bg-violet-950 text-white font-bold py-2 px-4  border rounded-xl mx-1 cursor-pointer transition-all">Edit</button>
-            <button className="bg-violet-800 hover:bg-violet-950 text-white font-bold py-2 px-4  border rounded-xl mx-1 cursor-pointer transition-all">Delete</button>
-          </div>
+        <div className="addTodo my-5">
+          <h2 className="text-lg font-bold">Add a Todo</h2>
+          <input onChange={handleChange} value={todo} type="text" className="w-80" />
+          <button onClick={handleAdd} className="bg-violet-800 hover:bg-violet-950 text-white font-bold py-2 px-4  border rounded-xl mx-3 cursor-pointer transition-all">Save</button>
+        </div>
+        <h2 className="text-lg font-bold">Your Todos</h2>
+        <div className="todos">
+          {todos.map(item => {
+
+            return <div key={item.id} my-3 className="todo flex w-1/4 justify-between" >
+              <input name={item.id} onChange={handleCheckbox} type="checkbox"  value={todo.isCompleted}/>
+              <div className={item.isCompleted ? "line-through" : ""}>{item.todo}</div>
+              <div className="buttons">
+                <button onClick={handleEdit} className="bg-violet-800 hover:bg-violet-950 text-white font-bold py-2 px-4  border rounded-xl mx-1 cursor-pointer transition-all">Edit</button>
+                <button onClick={handleDelete} className="bg-violet-800 hover:bg-violet-950 text-white font-bold py-2 px-4  border rounded-xl mx-1 cursor-pointer transition-all">Delete</button>
+              </div>
+            </div>
+          })}
         </div>
       </div>
-      </div>
-      
+
     </>
-    
+
   )
 }
 
